39e56fec1251c4bde803a075c388b942afd1b92f
Leander Thiele <lthiele@princeton.edu>
2022-10-24 13:18:11 -0400

    start to take additional steps between 99 and 79 to mitigate large neutrino mass problems

--- cmass.sh
+++ cmass.sh
@@ -10,6 +10,11 @@ export Z_MID=19.0
 export LOG_STEPS=5
 export LIN_STEPS=10
 
+# these are only relevant for large neutrino masses,
+# as recommended by Bayer+2020
+export EARLY_LOG_STEPS=10
+export EARLY_Z_MID=79.0
+
 # where we generate snapshots (order irrelevant, separate by spaces)
 export Z_OUT="0.44291191 0.45853203 0.47003594 0.47996333 0.48930041 0.49793992 0.50646687 0.51500717 0.52363417 0.53238288 0.54126877 0.55064664 0.56019118 0.57066313 0.58224299 0.59482622 0.60884719 0.62546052 0.64639842 0.67649879"
 

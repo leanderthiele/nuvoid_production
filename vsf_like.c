0d8e55fe9c33766d2c81fa99f9d8d27029f91e82
leander <leander97@gmx.de>
2022-10-31 13:13:20 -0400

    added small script to compute the VSF log-likelihood as we currently believe it to be correct

---
+++ vsf_like.c
@@ -0,0 +1,36 @@
+/* Compute the log-likelihood for a single augmentation
+ * relative to an observation.
+ * Command line arguments (all integers):
+ *   [1] N ... number of bins
+ *   [2...N+2] ... the observed counts
+ *   [N+3...2N+3] ... the simulated counts
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+
+#include <gsl/gsl_sf_gamma.h>
+
+int main(int argc, char **argv)
+{
+    char **c = argv+1;
+    unsigned N = atoi(*(c++));
+    unsigned xo[N], xs[N];
+    for (unsigned ii=0; ii<N; ++ii)
+        xo[ii] = atoi(*(c++));
+    for (unsigned ii=0; ii<N; ++ii)
+        xs[ii] = atoi(*(c++));
+    assert((c-argv)==argc);
+
+    // store in extended precision to minimize round-off during summation
+    long double out = 0.0L;
+
+    for (unsigned ii=0; ii<N; ++ii)
+        out += -(xo[ii]+xs[ii]) * M_LN2f64x + gsl_sf_lnchoose(xo[ii]+xs[ii], xo[ii]);
+
+    printf("%.16Le\n", out);
+
+    return 0;
+}

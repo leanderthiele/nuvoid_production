275ba63fa16f78f47cf46c1d2fa33a6cf81bb330
leander <leander97@gmx.de>
2022-11-01 13:41:27 -0400

    add openmp to compile_lightcone.sh

--- compile_lightcone.sh
+++ compile_lightcone.sh
@@ -3,4 +3,4 @@ g++ --std=c++17 -Wall -Wextra -O3 \
   -DHAVE_INLINE \
   -o lightcone lightcone.cpp cuboidremap-1.0/c++/cuboid.cpp \
   -L./pymangle/pymangle -L./healpix_lite \
-  -lm -lgsl -lgslcblas -lcmangle -lhealpixlite
+  -lm -lgsl -lgslcblas -lcmangle -lhealpixlite -fopenmp

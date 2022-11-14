5a220f7b4e422a1dfe1cbc87f25a4baa45c2f691
Leander Thiele <lthiele@princeton.edu>
2022-11-13 20:09:28 -0500

    lightcone segfault seems to come from healpix, compile that also with debugging symbols

--- healpix_lite/compile.sh
+++ healpix_lite/compile.sh
@@ -1,7 +1,7 @@
 for f in *.cc; do
   if [[ $f == _* ]]; then continue; fi
   echo $f
-  g++ --std=c++17 -Wall -Wextra -O3 -c -o ${f%.*}.o $f
+  g++ --std=c++17 -Wall -Wextra -O3 -ggdb -c -o ${f%.*}.o $f
 done
 
 ar -r -o libhealpixlite.a *.o

579cdb78fdd5e902d39aef00ec4fa031a34f6fd6
leander <leander97@gmx.de>
2022-11-16 13:52:20 -0500

    put debuginfos into mangle

--- pymangle/pymangle/compile.sh
+++ pymangle/pymangle/compile.sh
@@ -1,7 +1,7 @@
 for f in *.c; do
   if [[ $f == _* ]]; then continue; fi
   echo $f
-  gcc --std=gnu99 -Wall -Wextra -O3 -c -o ${f%.*}.o $f
+  gcc --std=gnu99 -Wall -Wextra -O3 -ggdb -c -o ${f%.*}.o $f
 done
 
 ar -r -o libcmangle.a *.o

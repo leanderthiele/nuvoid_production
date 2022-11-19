292f12b2ebd5766c589c8e58bed7f23bd8e583a0
Leander Thiele <lthiele@princeton.edu>
2022-11-18 19:27:35 -0500

    fixed small bug in clean_emulator.sh

--- clean_emulator.sh
+++ clean_emulator.sh
@@ -19,13 +19,11 @@ for d in "$root"/cosmo_varied_*/emulator/*; do
   # (someone may be working on it)
   if [ $(( now_time-create_time )) -lt $min_time_delta ]; then continue; fi
 
-  rm -f "$d"/lightcone_*.txt &
-  rm -rf "$d"/logs_* &
-  rm -rf "$d/figs" &
-
   # the following file is the last one VIDE generates
   if ls "$d"/sample_*/trimmed_nodencut_sky_positions_all_*.out 1> /dev/null 2>&1; then
-    continue
+    rm -f "$d"/lightcone_*.txt &
+    rm -rf "$d"/logs_* &
+    rm -rf "$d/figs" &
   else
     rm -r "$d" &
   fi

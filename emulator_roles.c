71a776a57067236fbdbbce06032109bbfbdbc69f
Leander Thiele <lthiele@princeton.edu>
2022-11-21 20:45:35 -0500

    emulator_roles is working!

--- emulator_roles.c
+++ emulator_roles.c
@@ -14,7 +14,7 @@
 struct ProcInfo
 {
     // inputs
-    int rank, rank_on_node;
+    int rank;
     uint64_t node_id;
 
     // computed
@@ -85,8 +85,8 @@ void assign_cosmo_indices (int N, int *out)
     for (int ii=0; ii<Ncosmo; ++ii)
     {
         cosmo_infos[ii].path = glob_result.gl_pathv[ii];
-        sprintf(buffer, "%s/emulator/*[a-f,0-9]", cosmo_infos[ii].path);
-        glob(buffer, GLOB_TILDE_CHECK | GLOB_ONLYDIR | GLOB_NOSORT, NULL, &glob_result1);
+        sprintf(buffer, "%s/emulator/*[a-f,0-9]/sample_*[0-9]/untrimmed_dencut_centers_central_*[0-9].out", cosmo_infos[ii].path);
+        glob(buffer, GLOB_TILDE_CHECK | GLOB_NOSORT, NULL, &glob_result1);
         cosmo_infos[ii].Nsamples = glob_result1.gl_pathc;
         cosmo_infos[ii].cosmo_idx = get_cosmo_idx(cosmo_infos[ii].path);
     }
@@ -133,25 +133,22 @@ int main(int argc, char **argv)
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
-    // the node-local rank (the node-local main will do the copying)
-    const int rank_on_node = atoi(getenv("SLURM_LOCALID"));
-    
     // figure out which node we are on (this is relative to this job!)
     const uint64_t node_id = get_nodeid();
+    #ifdef PRINT
     printf("%s %lu\n", getenv("SLURM_TOPOLOGY_ADDR"), node_id); // something like tiger-i26c2n2
+    #endif
 
     // gather info at root
-    int *rank_arr=0, *rank_on_node_arr=0;
+    int *rank_arr=0;
     uint64_t *node_id_arr=0;
     if (!rank)
     {
         rank_arr = (int *)malloc(world_size * sizeof(int));
-        rank_on_node_arr = (int *)malloc(world_size * sizeof(int));
         node_id_arr = (uint64_t *)malloc(world_size * sizeof(uint64_t));
     }
 
     MPI_Gather(&rank, 1, MPI_INT, rank_arr, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Gather(&rank_on_node, 1, MPI_INT, rank_on_node_arr, 1, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Gather(&node_id, 1, MPI_UINT64_T, node_id_arr, 1, MPI_UINT64_T, 0, MPI_COMM_WORLD);
 
     int *cosmo_idx_arr=0, *do_copying_arr=0, *rank2_arr=0;
@@ -163,7 +160,6 @@ int main(int argc, char **argv)
         {
             assert(rank_arr[ii] == ii); // this should be true I believe
             proc_infos[ii].rank = rank_arr[ii];
-            proc_infos[ii].rank_on_node = rank_on_node_arr[ii];
             proc_infos[ii].node_id = node_id_arr[ii];
         }
 
@@ -228,15 +224,15 @@ int main(int argc, char **argv)
     MPI_Finalize();
 
     #ifdef PRINT
-    printf("node=%lu\trank_on_node=%d\tcosmo_idx=%d\tdo_copying=%d\n",
-           node_id, rank_on_node, cosmo_idx, do_copying);
+    printf("node=%lu\tcosmo_idx=%d\tdo_copying=%d\n",
+           node_id, cosmo_idx, do_copying);
     #else
     printf("%d %d\n", cosmo_idx, do_copying);
     #endif
 
     if (!rank)
     {
-        free(rank_arr); free(rank_on_node_arr); free(node_id_arr);
+        free(rank_arr); free(node_id_arr);
         free(cosmo_idx_arr); free(do_copying_arr); free(rank2_arr);
     }
 }

3f6576c1dcc05ff8b6244c386d469406e5a7b9f2
Leander Thiele <lthiele@princeton.edu>
2022-11-29 20:39:28 -0500

    vsf_emulator_mlp.py working but not sure how well

--- vsf_emulator_mlp.py
+++ vsf_emulator_mlp.py
@@ -30,6 +30,12 @@ train_hists = hists[~validation_select]
 validation_params = params[validation_select]
 validation_hists = hists[validation_select]
 
+# normalize the inputs
+avg = np.mean(train_params, axis=0)
+std = np.std(train_params, axis=0)
+train_params = (train_params - avg[None, :]) / std[None, :]
+validation_params = (validation_params - avg[None, :]) / std[None, :]
+
 train_params = torch.from_numpy(train_params.astype(np.float32))
 train_hists = torch.from_numpy(train_hists.astype(np.float32))
 validation_params = torch.from_numpy(validation_params.astype(np.float32))
@@ -42,7 +48,7 @@ class MLPLayer(nn.Sequential) :
                                      ]))
 
 class MLP(nn.Sequential) :
-    def __init__(self, Nin, Nout, Nlayers=4, Nhidden=32) :
+    def __init__(self, Nin, Nout, Nlayers=8, Nhidden=1024) :
         # output is manifestly positive so we use ReLU in the final layer
         super().__init__(*[MLPLayer(Nin if ii==0 else Nhidden,
                                     Nout if ii==Nlayers else Nhidden,
@@ -60,22 +66,19 @@ class Loss(nn.Module) :
         pred_ = pred + 1e-8
 
         # negative log-likelihood under poisson distribution assumption
-        return - torch.sum(targ * torch.log(pred_) - pred_ - torch.special.gammaln(1.0+targ)) \
+        return - torch.sum(torch.special.xlogy(targ, pred_) - pred_ - torch.special.gammaln(1.0+targ)) \
                / pred.shape[0]
 
 train_set = TensorDataset(train_params, train_hists)
-validation_set = TensorDataset(validation_params, validation_hists)
-
-train_loader = DataLoader(train_set)
-validation_loader = DataLoader(validation_set)
+train_loader = DataLoader(train_set, batch_size=256)
 
 model = MLP(train_params.shape[1], train_hists.shape[1])
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 loss = Loss()
 
-for ii in range(10) :
+for ii in range(1000) :
     model.train()
-    for jj, (x, y) in enumerate(training_loader) :
+    for jj, (x, y) in enumerate(train_loader) :
         optimizer.zero_grad()
         ypred = model(x)
         l = loss(ypred, y)
@@ -89,4 +92,4 @@ for ii in range(10) :
     ypred = model(validation_params)
     lvalidation = loss(ypred, validation_hists)
 
-    print(f'{ltrain.item()}\t{lvalidation.item()}')
+    print(f'iteration {ii:4}: {ltrain.item():8.2f}\t{lvalidation.item():8.2f}')

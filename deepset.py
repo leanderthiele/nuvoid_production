0cc6a6eb2b605173064922ea347529c81d03b978
leander <leander97@gmx.de>
2022-12-07 13:53:11 -0500

    fixed the TODOs in deepset

--- deepset.py
+++ deepset.py
@@ -30,17 +30,23 @@ assert np.all(Nvoids == np.count_nonzero(redshifts>0, axis=1))
 assert all(np.all(x[:n]>0) for x, n in zip(radii, Nvoids))
 assert all(np.all(x[:n]>0) for x, n in zip(redshifts, Nvoids))
 
-# normalizations
-R_avg = np.mean(radii[radii>0])
-R_std = np.std(radii[radii>0])
-z_avg = np.mean(redshifts[radii>0])
-z_std = np.std(redshifts[radii>0])
+rng = np.random.default_rng(42)
+validation_select = rng.choice([True, False], size=len(radii), p=[VALIDATION_FRAC, 1-VALIDATION_FRAC])
+
+# normalizations -- don't cheat!
+train_radii = radii[~validation_select]
+train_redshifts = redshifts[~validation_select]
+train_N = Nvoids[~validation_select]
+R_avg = np.mean(train_radii[train_radii>0])
+R_std = np.std(train_radii[train_radii>0])
+z_avg = np.mean(train_redshifts[train_redshifts>0])
+z_std = np.std(train_redshifts[train_redshifts>0])
+N_avg = np.mean(train_N.astype(float))
+N_std = np.std(train_N.astype(float))
 
 # shape [Nsamples, void index, 2]
 data = np.stack([(radii-R_avg)/R_std, (redshifts-z_avg)/z_std], axis=-1)
-
-rng = np.random.default_rng(42)
-validation_select = rng.choice([True, False], size=len(radii), p=[VALIDATION_FRAC, 1-VALIDATION_FRAC])
+norm_Nvoids = (Nvoids.astype(float) - N_avg) / N_std
 
 # TARGET_IDX = 5 # M_nu
 TARGET_IDX = 15 # mu_Mmin
@@ -48,35 +54,39 @@ TARGET_IDX = 15 # mu_Mmin
 train_params = params[~validation_select]
 train_data = data[~validation_select]
 train_Nvoids = Nvoids[~validation_select]
+train_norm_Nvoids = norm_Nvoids[~validation_select]
 validation_params = params[validation_select]
 validation_data = data[validation_select]
 validation_Nvoids = Nvoids[validation_select]
+validation_norm_Nvoids = norm_Nvoids[validation_select]
 
 # to torch
 train_params = torch.from_numpy(train_params.astype(np.float32)).to(device=device)
 train_data = torch.from_numpy(train_data.astype(np.float32)).to(device=device)
 train_Nvoids = torch.from_numpy(train_Nvoids.astype(np.int32)).to(device=device)
+train_norm_Nvoids = torch.from_numpy(train_norm_Nvoids.astype(np.float32)).to(device=device)
 validation_params = torch.from_numpy(validation_params.astype(np.float32)).to(device=device)
 validation_data = torch.from_numpy(validation_data.astype(np.float32)).to(device=device)
-validation_Nvoids = torch.from_numpy(validation_Nvoids.astype(np.int32)).to(device=device)
+validation_Nvoids = torch.from_numpy(validation_Nvoids.astype(np.int32)).to(device=device).unsqueeze(-1)
+validation_norm_Nvoids = torch.from_numpy(validation_norm_Nvoids.astype(np.float32)).to(device=device).unsqueeze(-1)
 
 train_target = train_params[:, TARGET_IDX].unsqueeze(-1)
 validation_target = validation_params[:, TARGET_IDX].unsqueeze(-1)
 
 class MLPLayer(nn.Sequential) :
-    def __init__(self, Nin, Nout, activation=nn.LeakyReLU) :
+    def __init__(self, Nin, Nout, activation=True) :
         super().__init__(OrderedDict([('linear', nn.Linear(Nin, Nout, bias=True)),
-                                      ('activation', activation()),
+                                      ('activation', nn.LeakyReLU() if activation else nn.Identity()),
                                      ]))
 
 class MLP(nn.Sequential) :
-    def __init__(self, Nin, Nout, Nlayers=2, Nhidden=256, out_positive=True) :
+    def __init__(self, Nin, Nout, Nlayers=2, Nhidden=256, last_activation=True) :
         # output is manifestly positive so we use ReLU in the final layer
         self.Nin = Nin
         self.Nout = Nout
         super().__init__(*[MLPLayer(Nin if ii==0 else Nhidden,
                                     Nout if ii==Nlayers else Nhidden,
-                                    activation=(nn.LeakyReLU if (ii!=Nlayers or not out_positive) else nn.ReLU))
+                                    activation=ii!=Nlayers or last_activation)
                            for ii in range(Nlayers+1)])
 
 class DeepSetLayer(nn.Module) :
@@ -102,27 +112,26 @@ class DeepSetLayer(nn.Module) :
         return x
 
 class DeepSet(nn.Module) :
-    def __init__(self, Nin, Nout, Ncontext, *args, **kwargs) :
-        # Ncontext should be a list of intermediate context dimensions, or None if only a single DeepSetLayer
-        # should be used
-        # TODO we need to use the overall number of voids as a global context!!!
-        # TODO also need to be more careful with the out_positive thing, probably just get rid of it
-        #      and remove activation in final layer
+    def __init__(self, Nin, Nout, Nlatent, Ncontext, Nds, *args, **kwargs) :
         self.Nin = Nin
         self.Nout = Nout
-        self.Ncontext = list() if Ncontext is None else Ncontext
-        self.Nlayers = len(self.Ncontext)+1
+        self.Nlatent = Nlatent
+        self.Ncontext = Ncontext
+        self.Nds = Nds
         super().__init__()
-        self.layers = nn.ModuleList([DeepSetLayer(Nin,
-                                                  Nout if ii==self.Nlayers-1 else self.Ncontext[ii],
-                                                  other_dim=None if ii==0 else self.Ncontext[ii-1],
+        self.layers = nn.ModuleList([DeepSetLayer(Nin, self.Nlatent,
+                                                  other_dim=None if ii==0 else self.Nlatent,
                                                   *args, **kwargs)
-                                     for ii in range(self.Nlayers)])
-    def forward(self, x, N) :
+                                     for ii in range(self.Nds)])
+        self.collapse = MLP(self.layers[-1].mlp.Nout+Ncontext, Nout, last_activation=False, *args, **kwargs)
+    def forward(self, x, N, context) :
+        # context is the global context which is added to the final MLP input, shape [batch, feature]
         latent = None
         for l in self.layers :
             latent = l(x, N, latent)
-        return latent
+        # latent has shape [batch, features]
+        latent = torch.cat([latent, context], dim=-1)
+        return self.collapse(latent)
 
 class Loss(nn.Module) :
     # simple MSE at the moment
@@ -138,18 +147,18 @@ loss = Loss()
 model = DeepSet(data.shape[-1], 1, [16,]).to(device=device)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
 
-train_set = TensorDataset(train_data, train_Nvoids, train_target)
+train_set = TensorDataset(train_data, train_Nvoids, train_norm_Nvoids, train_target)
 train_loader = DataLoader(train_set, batch_size=256)
-validation_set = TensorDataset(validation_data, validation_Nvoids, validation_target)
+validation_set = TensorDataset(validation_data, validation_Nvoids, validation_norm_Nvoids, validation_target)
 validation_loader = DataLoader(validation_set, batch_size=2048)
 
 for epoch in range(EPOCHS) :
     
     model.train()
     ltrain = []
-    for ii, (x, n, y) in enumerate(train_loader) :
+    for ii, (x, n, nnorm, y) in enumerate(train_loader) :
         optimizer.zero_grad()
-        pred = model(x, n)
+        pred = model(x, n, nnorm)
         l = loss(pred, y)
         ltrain.append(l.item())
         l.backward()
@@ -160,8 +169,8 @@ for epoch in range(EPOCHS) :
     model.eval()
 
     lvalidation = []
-    for ii, (x, n, y) in enumerate(validation_loader) :
-        pred = model(x, n)
+    for ii, (x, n, nnorm, y) in enumerate(validation_loader) :
+        pred = model(x, n, nnorm)
         l = loss(pred, y)
         lvalidation.append(l.item())
 

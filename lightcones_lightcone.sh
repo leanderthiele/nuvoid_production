7087e4f5b608d34bdd4faffab46d9718f433cc97
leander <leander97@gmx.de>
2022-12-14 12:14:05 -0500

    generate a bunch of lightcones per sample as it is super cheap

--- lightcones_lightcone.sh
+++ lightcones_lightcone.sh
@@ -7,6 +7,9 @@
 #   [4] augmentation (0..95)
 #   [5] halo finder (rockstar or rfof)
 
+# how many lightcones we generate per call (with different augmentations)
+NSAMPLES=8
+
 set -e -o pipefail
 
 codebase="${HOME}/nuvoid_production"
@@ -36,8 +39,6 @@ binary_output=1
 Omega_m=$(grep -m1 -oP 'Omega\_m=+\K\d\.\d*' "$data_dir/cosmo.info")
 BoxSize=$(grep -m1 -oP 'boxsize\s=\s+\K\d*(\.\d*)?' "$data_dir/fastpm_script.lua")
 comma_snap_times=$($SNAP_TIMES_EXE $data_dir $halo_finder)
-remap=$((augment / 48))
-reflecttranslate=$((augment % 48))
 
 # run the code
 module load $LIGHTCONE_MODULES
@@ -50,14 +51,25 @@ ulimit -c unlimited
 # if they do get generated
 cd '/scratch/gpfs/lthiele/nuvoid_production/lightcone_core_files'
 
-$LIGHTCONE_EXE \
-  "$wrk_dir/emulator/$hod_hash" "" \
-  $augment \
-  $BoxSize $Omega_m $zmin $zmax \
-  $remap $correct $reflecttranslate \
-  $boss_dir $veto $stitch_before_RSD \
-  $verbose $binary_output \
-  $(echo $comma_snap_times | tr ',' ' ')
+# we can generate a bunch of samples as this is super cheap
+this_augment=$augment
+for ii in $( seq 1 $NSAMPLES ); do
+
+  remap=$((this_augment / 48))
+  reflecttranslate=$((this_augment % 48))
+  $LIGHTCONE_EXE \
+    "$wrk_dir/emulator/$hod_hash" "" \
+    $this_augment \
+    $BoxSize $Omega_m $zmin $zmax \
+    $remap $correct $reflecttranslate \
+    $boss_dir $veto $stitch_before_RSD \
+    $verbose $binary_output \
+    $(echo $comma_snap_times | tr ',' ' ')
+
+  # this works in covering the entire ring, and it gives
+  # enough entropy that we shouldn't really worry
+  this_augment=$(( (this_augment+37) % 96 ))
+done
 
 # and reset to avoid side effects
 ulimit -c 0

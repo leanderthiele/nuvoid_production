3cd7b302d523e01f7e375f53ab89fd15f1784d49
leander <leander97@gmx.de>
2022-12-14 23:15:39 -0500

    hopefully lightcones_plk.py syncs properly now

--- lightcones_plk.py
+++ lightcones_plk.py
@@ -9,6 +9,9 @@ import nbodykit.lab as NBL
 
 from plk import PLKCalc
 
+comm_world = MPI.COMM_WORLD
+world_rank = comm_world.Get_rank()
+
 codebase = '/home/lthiele/nuvoid_production'
 CPUS_PER_TASK = int(os.environ['CPUS_PER_TASK'])
 
@@ -24,33 +27,45 @@ with NBL.TaskManager(cpus_per_task=CPUS_PER_TASK) as tm :
     for ii in tm.iterate(list(range(10000))) :
         
         # get the sample we should work on
-        result = subprocess.run(f'{codebase}/mysql_driver create_plk',
-                                shell=True, capture_output=True, check=True)
+        if tm.comm.rank == 0 :
 
-        # these are strings!
-        cosmo_idx, hod_idx, hod_hash = result.stdout.strip().decode().split()
-        if int(cosmo_idx) < 0 :
-            # no work left
-            continue
+            result = subprocess.run(f'{codebase}/mysql_driver create_plk',
+                                    shell=True, capture_output=True, check=True)
 
-        subprocess.run(f'{codebase}/mysql_driver start_plk {cosmo_idx} {hod_idx}',
-                       shell=True, check=True)
+            # these are strings!
+            cosmo_idx, hod_idx, hod_hash = result.stdout.strip().decode().split()
+            if int(cosmo_idx) < 0 :
+                # no work left
+                continue
 
-        # this is where we operate
-        wrk_dir = f'/scratch/gpfs/lthiele/nuvoid_production/cosmo_varied_{cosmo_idx}/lightcones/{hod_hash}'
+            subprocess.run(f'{codebase}/mysql_driver start_plk {cosmo_idx} {hod_idx}',
+                           shell=True, check=True)
 
-        lightcone_files = glob(f'{wrk_dir}/lightcone_[0-9]*.bin')
-        if not lightcone_files :
-            print(f'No lightcones found in {wrk_dir}!', file=sys.stderr)
-            report_state(cosmo_idx, hod_idx, 1)
-            continue
+            # this is where we operate
+            wrk_dir = f'/scratch/gpfs/lthiele/nuvoid_production/cosmo_varied_{cosmo_idx}/lightcones/{hod_hash}'
+
+            lightcone_files = glob(f'{wrk_dir}/lightcone_[0-9]*.bin')
+            if not lightcone_files :
+                print(f'No lightcones found in {wrk_dir}!', file=sys.stderr)
+                report_state(cosmo_idx, hod_idx, 1)
+                continue
+
+        print(f'RANK {world_rank} working on {ii}')
+
+        # let everyone on the team know which files we should operate on
+        lightcone_files = tm.comm.bcast(lightcone_files)
+
+        print(f'RANK {world_rank} working on {lightcone_files[0]}...')
 
         # iterate over the lightcones
         for lightcone_file in lightcone_files :
             
-            augment = re.search('(?<=lightcone_)[0-9]*', lightcone_file)[0]
             plk_result = plk_calc.compute_from_fname(lightcone_file)
-            np.savez(f'{wrk_dir}/plk_{augment}.npz', **plk_result)
+
+            if tm.comm.rank == 0 :
+                augment = re.search('(?<=lightcone_)[0-9]*', lightcone_file)[0]
+                np.savez(f'{wrk_dir}/plk_{augment}.npz', **plk_result)
 
         # all done!
-        report_state(cosmo_idx, hod_idx, 0)
+        if tm.comm.rank == 0 :
+            report_state(cosmo_idx, hod_idx, 0)

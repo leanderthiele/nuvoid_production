1cc151a72afe1ec22558dbe5f79f21b22600d6dc
leander <leander97@gmx.de>
2022-12-27 17:43:49 -0500

    added initial code to compute voids for the fiducial lightcones

---
+++ fiducials_lightcones_voids.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Command line arguments:
+#   [1] version index
+
+set -e -o pipefail
+
+version="$1"
+
+codebase="$HOME/nuvoid_production"
+
+MYSQL_DRIVER="$codebase/mysql_driver"
+
+# these take input, output directory as command line arguments
+BIN_TXT="python $codebase/lightcones_convert_bin_txt.py"
+VIDE_SH="bash $codebase/lightcones_vide.sh"
+
+# loop until time is up
+for ii in $( seq 1 10000 ); do
+  # get work
+  read running_idx seed_idx lightcone_idx hod_hash <<< "$($MYSQL_DRIVER 'create_fiducials_voids' $version)"
+  if [ $seed_idx -lt 0 ]; then
+    break
+  fi
+
+  # we are starting
+  $MYSQL_DRIVER 'start_fiducials_voids' $version $running_idx $seed_idx $lightcone_idx
+
+  perm_dir="/scratch/gpfs/lthiele/nuvoid_production/cosmo_fiducial_${seed_idx}/lightcones/${hod_hash}"
+  tmp_dir="/tmp/cosmo_fiducial_${seed_idx}/lightcones/${hod_hash}"
+  mkdir -p $tmp_dir
+
+  # convert .bin -> .txt
+  module load anaconda3/2021.11
+  $BIN_TXT $perm_dir $tmp_dir \
+    && status=$? || status=$?
+  module rm anaconda3/2021.11
+  if [ $status -ne 0 ]; then
+    $MYSQL_DRIVER 'end_fiducials_voids' $version $running_idx $seed_idx $lightcone_idx $status
+    continue
+  fi
+
+  # find voids
+  $VIDE_SH $tmp_dir $perm_dir \
+    && status=$? || status=$?
+  $MYSQL_DRIVER 'end_fiducials_voids' $version $running_idx $seed_idx $lightcone_idx $status
+done

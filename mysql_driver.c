2ac58db5f1e613d7e49631e7a5a66328b75a333a
leander <leander97@gmx.de>
2022-12-29 03:40:03 -0500

    add vgplk_state to get_run

--- mysql_driver.c
+++ mysql_driver.c
@@ -117,7 +117,7 @@ possible commands:
     argv[2] = minutes
 [get_run]
     argv[2] = hod_idx
-    returns cosmo_idx hod_hash state plk_state voids_state
+    returns cosmo_idx hod_hash state plk_state voids_state vgplk_state
     cosmo_idx == -1 if nothing left
 )"""";
 
@@ -815,11 +815,13 @@ void new_fiducials_lightcones (MYSQL *p, int version)
     }
 }
 
-int get_run (MYSQL *p, uint64_t hod_idx, char *hod_hash, char *state, char *plk_state, char *voids_state)
+int get_run (MYSQL *p, uint64_t hod_idx, char *hod_hash, char *state,
+             char *plk_state, char *voids_state, char *vgplk_state)
 {
     char query_buffer[1024];
     MYSPRINTF(query_buffer,
-              "SELECT cosmo_idx, hod_hash, state, plk_state, voids_state FROM lightcones "
+              "SELECT cosmo_idx, hod_hash, state, plk_state, voids_state, vgplk_state "
+              "FROM lightcones "
               "WHERE hod_idx=%lu",
               hod_idx);
 
@@ -835,7 +837,7 @@ int get_run (MYSQL *p, uint64_t hod_idx, char *hod_hash, char *state, char *plk_
     assert(num_rows==1);
 
     unsigned int num_fields = mysql_num_fields(query_res);
-    assert(num_fields==5);
+    assert(num_fields==6);
     MYSQL_ROW row = mysql_fetch_row(query_res);
     int cosmo_idx = atoi(row[0]);
 
@@ -844,6 +846,7 @@ int get_run (MYSQL *p, uint64_t hod_idx, char *hod_hash, char *state, char *plk_
     sprintf(state, "%s", (row[2]) ? row[2] : "NULL");
     sprintf(plk_state, "%s", (row[3]) ? row[3] : "NULL");
     sprintf(voids_state, "%s", (row[4]) ? row[4] : "NULL");
+    sprintf(vgplk_state, "%s", (row[5]) ? row[5] : "NULL");
 
     mysql_free_result(query_res);
 
@@ -994,9 +997,9 @@ int main(int argc, char **argv)
     }
     else if (!strcmp(mode, "get_run"))
     {
-        char hod_hash[40], state[40], plk_state[40], voids_state[40];
-        int cosmo_idx = get_run(&p, atoll(argv[2]), hod_hash, state, plk_state, voids_state);
-        fprintf(stdout, "%d %s %s %s %s\n", cosmo_idx, hod_hash, state, plk_state, voids_state);
+        char hod_hash[40], state[40], plk_state[40], voids_state[40], vgplk_state[40];
+        int cosmo_idx = get_run(&p, atoll(argv[2]), hod_hash, state, plk_state, voids_state, vgplk_state);
+        fprintf(stdout, "%d %s %s %s %s %s\n", cosmo_idx, hod_hash, state, plk_state, voids_state, vgplk_state);
     }
     else if (!strcmp(mode, "new_fiducials_lightcones"))
     {

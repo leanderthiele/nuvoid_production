7425c50dbaa71bfe09d6de738d274d7fb6b702bd
leander <leander97@gmx.de>
2023-02-08 16:15:07 -0500

    split off unpickling of posterior into separate file

--- lfi_sample.py
+++ lfi_sample.py
@@ -1,6 +1,4 @@
 from sys import argv
-import io
-import pickle
 import re
 import os
 from multiprocessing import cpu_count
@@ -14,6 +12,7 @@ import sbi.inference
 from sbi import utils as sbi_utils
 
 from read_txt import read_txt
+from lfi_load_posterior import load_posterior
 
 SAMPLE_SHAPE = 20000
 
@@ -50,30 +49,8 @@ model_ident = match[3]
 
 model_fname = f'{filebase}/{model_fname_base}'
 
-class Unpickler(pickle.Unpickler) :
-    """ small utility to load cross-device """
-    def find_class (self, module, name) :
-        if module == 'torch.storage' and name == '_load_from_bytes' :
-            return lambda b: torch.load(io.BytesIO(b), map_location=device)
-        else :
-            return super().find_class(module, name)
-
 # load the settings and model from file
-settings_str = b''
-with open(model_fname, 'rb') as f :
-    while True :
-        # scan until we reach newline, which indicates start of the pickled model
-        c = f.read(1)
-        if c == b'\n' :
-            break
-        settings_str += c
-    posterior = Unpickler(f).load()
-
-SETTINGS = eval(settings_str)
-
-# fix some stuff
-posterior._device = device
-posterior.potential_fn.device = device
+SETTINGS, posterior = load_posterior(model_fname, device)
 
 # load and compress the observation
 compress_fname = f'{filebase}/compression_v{version}_{compression_hash}.dat'

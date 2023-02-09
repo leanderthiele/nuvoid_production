817b9a0b50f6e2834aff53f4a29c1d15b5f62144
leander <leander97@gmx.de>
2023-02-09 16:13:57 -0500

    make plot_posterior.py

--- plot_posterior.py
+++ plot_posterior.py
@@ -1,3 +1,5 @@
+# TODO colors, legend, outfile name
+
 from sys import argv
 import os.path
 from glob import glob
@@ -24,54 +26,57 @@ fsruns = [
 
 DISCARD = 1000
 
-chain_fname_base = argv[1]
-chain_fname_base_root, _ = os.path.splitext(chain_fname_base)
+# can pass multiple
+chain_fname_bases = argv[1:]
 
-chain_fname = f'{filebase}/{chain_fname_base}'
-with np.load(chain_fname) as f :
-    chain = f['chain']
-    logprob = f['log_prob']
-    param_names = list(f['param_names'])
-assert param_names[0] == 'Mnu'
-DIM = len(param_names)
-assert chain.shape[-1] == DIM
-chain = chain[DISCARD:, ...].reshape(-1, DIM)
-logprob = logprob[DISCARD:, ...].flatten()
+# get the first one from corner
+fig = None
+a_leg = None
+DIM = None
+param_names = None
 
-fig = corner.corner(chain, labels=param_names, plot_datapoints=False, color='black')
+for chain_fname_base in chain_fname_bases :
+    chain_fname_base_root, _ = os.path.splitext(chain_fname_base)
 
-if HAVE_FS or HAVE_PROFILE :
+    chain_fname = f'{filebase}/{chain_fname_base}'
+    with np.load(chain_fname) as f :
+        chain = f['chain']
+        logprob = f['log_prob']
+        param_names_ = list(f['param_names'])
+    if param_names is None :
+        param_names = param_names_
+    else :
+        assert param_names == param_names_
+    assert param_names[0] == 'Mnu'
+    dim_ = len(param_names)
+    if DIM is None :
+        DIM = dim_
+    else :
+        assert DIM == dim_
+    assert chain.shape[-1] == DIM
+    chain = chain[DISCARD:, ...].reshape(-1, DIM)
+    logprob = logprob[DISCARD:, ...].flatten()
+
+    fig = corner.corner(chain, labels=param_names,
+                        plot_datapoints=False, plot_density=False, no_fill_contours=True,
+                        levels=1 - np.exp(-0.5 * np.array([1, 2])**2), # values in array are sigmas
+                        fig=fig)
+
     # this is a bit hacky and depends on how corner implements stuff...
     a = fig.axes[0]
-    p = a.patches[-1]
-    xedges = p.xy[:, 0][::2]
-    yvalues = p.xy[:, 1][1:-1:2]
-    N = np.sum(yvalues)
-    xcenters = 0.5*(xedges[1:] + xedges[:-1])
-    if HAVE_FS :
-        for fsrun in fsruns :
-            fsbase = f'{fsroot}/{fsrun}'
+    p = a.patches[-1] # get the last one
+    color = p.get_edgecolor()
 
-            fs_param_names_files = glob(f'{fsbase}/*.paramnames')
-            assert len(fs_param_names_files) == 1
-            Mnu_idx = None
-            with open(fs_param_names_files[0], 'r') as f :
-                for ii, line in enumerate(f) :
-                    if line.startswith('M_tot') :
-                        Mnu_idx = ii
-                        break
-            assert Mnu_idx is not None
-            fs_Mnu = np.empty(0)
+    # this is where we put the legend
+    if a_leg is None :
+        a_leg = fig.axes[DIM-1]
+    a_leg.hist(np.random.rand(2), label=chain_fname_base_root, color=color)
 
-            fs_txt_files = glob(f'{fsbase}/*.txt')
-            for fs_txt_file in fs_txt_files :
-                repeats, fs_Mnu_ = np.loadtxt(fs_txt_file, usecols=(0, 2+Mnu_idx), unpack=True)
-                fs_Mnu_ = np.repeat(fs_Mnu_, repeats.astype(int))[200:] # discard burn-in
-                fs_Mnu = np.concatenate((fs_Mnu, fs_Mnu_))
-            h, e = np.histogram(fs_Mnu, bins=xedges)
-            h = h.astype(float) * N / np.sum(h)
-            a.plot(xcenters, h, linestyle='dashed')
     if HAVE_PROFILE :
+        xedges = p.xy[:, 0][::2]
+        yvalues = p.xy[:, 1][1:-1:2]
+        N = np.sum(yvalues)
+        xcenters = 0.5*(xedges[1:] + xedges[:-1])
         mnu = chain[:, 0]
         bin_indices = np.digitize(mnu, xedges) - 1
         avg_logprob = np.empty(len(xedges)-1)
@@ -80,10 +85,48 @@ if HAVE_FS or HAVE_PROFILE :
         avg_logprob -= np.max(avg_logprob)
         avg_prob = np.exp(avg_logprob)
         avg_prob *= np.max(yvalues)
-        a.plot(xcenters, avg_prob, linestyle='dotted', color='black')
+        a.plot(xcenters, avg_prob, linestyle='dotted', color=color)
+
+# get the legend
+a_leg.set_xlim(10,11)
+a_leg.legend()
+
+if HAVE_FS :
+    # this is a bit hacky and depends on how corner implements stuff...
+    a = fig.axes[0]
+    p = a.patches[-1]
+    xedges = p.xy[:, 0][::2]
+    yvalues = p.xy[:, 1][1:-1:2]
+    N = np.sum(yvalues)
+    xcenters = 0.5*(xedges[1:] + xedges[:-1])
+    for fsrun in fsruns :
+        fsbase = f'{fsroot}/{fsrun}'
+
+        fs_param_names_files = glob(f'{fsbase}/*.paramnames')
+        assert len(fs_param_names_files) == 1
+        Mnu_idx = None
+        with open(fs_param_names_files[0], 'r') as f :
+            for ii, line in enumerate(f) :
+                if line.startswith('M_tot') :
+                    Mnu_idx = ii
+                    break
+        assert Mnu_idx is not None
+        fs_Mnu = np.empty(0)
+
+        fs_txt_files = glob(f'{fsbase}/*.txt')
+        for fs_txt_file in fs_txt_files :
+            repeats, fs_Mnu_ = np.loadtxt(fs_txt_file, usecols=(0, 2+Mnu_idx), unpack=True)
+            fs_Mnu_ = np.repeat(fs_Mnu_, repeats.astype(int))[200:] # discard burn-in
+            fs_Mnu = np.concatenate((fs_Mnu, fs_Mnu_))
+        h, e = np.histogram(fs_Mnu, bins=xedges)
+        h = h.astype(float) * N / np.sum(h)
+        a.plot(xcenters, h, linestyle='dashed')
 
 
 # for ii, name in enumerate(param_names) :
 #     ax[ii, ii].set_title(name)
 
-fig.savefig(f'{filebase}/{chain_fname_base_root}.pdf', bbox_inches='tight')
+ident, _ = os.path.splitext(chain_fname_bases[0])
+if len(chain_fname_bases) > 0 :
+    ident = f'{ident}_and{len(chain_fname_bases)-1}'
+fig.savefig(f'{filebase}/{ident}.pdf', bbox_inches='tight')

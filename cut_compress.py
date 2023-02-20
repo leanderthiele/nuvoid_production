d9c0cf7299949114e55330a07d9ec1ba3335e70e
leander <leander97@gmx.de>
2023-02-19 19:46:59 -0500

    add option to cut_compress.py to not do compression

--- cut_compress.py
+++ cut_compress.py
@@ -11,14 +11,26 @@ class CutCompress :
             if not cut_kwargs['have_Cprior'] :
                 Cprior = None
             del cut_kwargs['have_Cprior']
+
+        if 'do_compression' in cut_kwargs :
+            self.do_compression = cut_kwargs['do_compression']
+            del cut_kwargs['do_compression']
+        else :
+            self.do_compression = True
+
         self.cut = Cut(**cut_kwargs)
 
-        cut_dm_dphi = self.cut.cut_vec(dm_dphi)
         cut_C = self.cut.cut_mat(C)
 
-
-        self.compress = Compress(cut_dm_dphi, cut_C, is_nuisance, Cprior)
-        self.cut_compression_matrix = self.cut.expand_vec(self.compress.compression_matrix, fill_value=0.0)
+        if self.do_compression :
+            cut_dm_dphi = self.cut.cut_vec(dm_dphi)
+            self.compress = Compress(cut_dm_dphi, cut_C, is_nuisance, Cprior)
+            self.cut_compression_matrix = self.cut.expand_vec(self.compress.compression_matrix, fill_value=0.0)
+        else :
+            # no compression, but we still do the transformation such that the covariance
+            # becomes the identity
+            L = np.linalg.cholesky(cut_C)
+            self.cut_compression_matrix = self.cut.expand_vec(np.linalg.inv(L), fill_value=0.0)
 
     def __call__ (self, x) :
         return np.einsum('ai,...i->...i', self.cut_compression_matrix, x)

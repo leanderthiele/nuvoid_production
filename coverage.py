06dbb15266bff8df11d0769f6db4976e7665aa8f
Leander Thiele <lthiele@princeton.edu>
2023-02-23 03:00:51 -0500

    coverage working very well

--- coverage.py
+++ coverage.py
@@ -36,12 +36,13 @@ def oneminusalpha (samples, theta, prior) :
     std = np.std(samples)
     
     # approximate continuous posterior
+    # choice of bandwidth is visual heuristic, but should hopefully not matter too much
     kde = KernelDensity(kernel='epanechnikov', bandwidth=0.1*std).fit(samples.reshape(-1, 1))
     nll = lambda x : -kde.score_samples(np.array([[x]]).reshape(-1,1))
 
     # get some feel for the posterior
     edges = np.linspace(*prior, num=501)
-    centers = 0.5 * (bins[1:] + bins[:-1])
+    centers = 0.5 * (edges[1:] + edges[:-1])
     h, _ = np.histogram(samples, bins=edges)
     x0 = centers[np.argmax(h)]
     xlo = max((prior[0], x0-std))
@@ -53,7 +54,7 @@ def oneminusalpha (samples, theta, prior) :
 
     # find maximum posterior so we know in which direction to go
     try :
-        sln = basinhopping(nll, x0, T=0.1, niter=10, minimizer_kwargs={'bounds': [(xlo, xhi), ])
+        sln = basinhopping(nll, x0, T=0.1, niter=10, minimizer_kwargs={'bounds': [(xlo, xhi), ]})
     except ValueError : # rare case
         print(f'*** basinhopping failed')
         return -1
@@ -70,7 +71,7 @@ def oneminusalpha (samples, theta, prior) :
         bounds = (xmax, np.max(samples)) if side=='l' else (np.min(samples), xmax)
         try :
             sln = root_scalar(ftarg, bracket=bounds)
-            xsln = sln.x
+            xsln = sln.root
         except Exception as e :
             if str(e) == 'f(a) and f(b) must have different signs' :
                 # extremely rare case in which theta is exactly at the maximum posterior
@@ -98,24 +99,32 @@ def job (chain_fname) :
     oma = [oneminusalpha(x, theta, prior) for x, theta, prior \
            in zip(chain.T, real_params, priors)]
 
-    print(f'obs_idx={obs_idx} done!')
     return obs_idx, cosmo_idx, oma
 
 
 if __name__ == '__main__' :
     
-    batch_idx = 0
-    while True : # keep looping over batches
+    num_batches = int(np.ceil(len(chain_fnames) / batch_size))
+
+    obs_idx = []
+    cosmo_idx = []
+    oma = []
+
+    for batch_idx in range(num_batches) : # keep looping over batches
         
         these_chain_fnames = chain_fnames[batch_idx*batch_size : (batch_idx+1)*batch_size]
+
         with mp.Pool(num_workers) as pool :
             results = pool.map(job, these_chain_fnames)
 
-        obs_idx = [r[0] for r in results]
-        cosmo_idx = [r[1] for r in results]
-        oma=[r[2] for r in results]
+        obs_idx.extend(r[0] for r in results)
+        cosmo_idx.extend(r[1] for r in results)
+        oma.extend(r[2] for r in results)
 
-        np.savez(f'{filebase}/oneminusalpha_v{version}_{compression_hash}_{model_ident}_batch{batch_idx}.npz',
+        np.savez(f'{filebase}/oneminusalpha_v{version}_{compression_hash}_{model_ident}.npz',
+                 param_names=param_names,
                  obs_idx=np.array(obs_idx),
                  cosmo_idx=np.array(cosmo_idx),
                  oneminusalpha=np.array(oma))
+
+        print(f'Batch {batch_idx} / {num_batches} done!')

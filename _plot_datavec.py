38638e5a8c55a0483e07e45b528245982653e98f
Leander Thiele <lthiele@princeton.edu>
2023-12-12 18:38:19 -0500

    added quick script to inspect standard deviations (this is a test regarding the augmentation stuff)

--- _plot_datavec.py
+++ _plot_datavec.py
@@ -1,5 +1,6 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from matplotlib.ticker import FixedLocator
 
 from read_txt import read_txt
 from cut import Cut, VSF_REDGES, VGPLK_K, PLK_K
@@ -10,7 +11,6 @@ filebase = '/tigress/lthiele/nuvoid_production'
 target_data = np.loadtxt(f'{filebase}/datavector_CMASS_North_wweight.dat')
 target_data_wweight = np.loadtxt(f'{filebase}/datavector_CMASS_North_wweight2.dat')
 xindices = np.arange(len(target_data))
-figsize = (12, 3)
 
 # these are the best fit trial indices (averaged) for the cut data vectors
 # fit to plk and vsf only
@@ -29,12 +29,21 @@ plk_k_ticks = [0.05, 0.15]
 vsf_R_labels = (55, '$R$ [Mpc/$h$]')
 plk_k_labels = (0.1,'$k$ [$h$/Mpc]')
 
-def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, **plot_kwargs) :
+def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, have_delta=False, **plot_kwargs) :
     # plots into [-1, 1] range
     
     if ax is None :
-        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=figsize)
+        figsize = (12, 3)
+        if have_delta :
+            figsize = (figsize[0], figsize[1]*1.4)
+        fig, ax = plt.subplots(nrows=1+int(have_delta), ncols=1, figsize=figsize,
+                               gridspec_kw=dict(hspace=0.3, height_ratios=[2,1]))
+        if have_delta :
+            assert have_bf
+            ax_delta = ax[1]
+            ax = ax[0]
     else :
+        assert not have_delta
         fig = None
 
     cut_vsf = Cut(use_vsf=True, use_vgplk=False, use_plk=False)
@@ -82,16 +91,45 @@ def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, **p
 
     ax.set_ylim(-1, 1)
     ax.set_xlim(-0.5, len(target_data)-0.5)
+    ax.set_ylabel('datavector (rescaled)')
+
+    if have_delta :
+        ax_delta.set_xlim(*ax.get_xlim())
+        lim = 12
+        linthresh = 3
+        ax_delta.set_yscale('symlog', linthresh=3)
+        ax_delta.set_ylim(-lim, lim)
+        ax_delta.set_yticks([-lim, -linthresh, 0, linthresh, lim, ])
+        ax_delta.set_ylabel('$\Delta / \sigma$')
+        ax_delta.set_yticklabels([str(-lim), str(-linthresh), '0', str(linthresh), str(lim), ])
+        ax_delta.axhline(0, color='grey', linestyle='dashed')
+        minor_locator = FixedLocator([-2, -1, 1, 2])
+        ax_delta.yaxis.set_minor_locator(minor_locator)
+        for sgn in [+1, -1] :
+            ax_delta.axhline(sgn * linthresh, color='grey', linestyle='dotted')
 
 #    ax.plot(xindices, y, linestyle='none', marker='o', label='CMASS NGC OLD', **plot_kwargs)
     ax.plot(xindices, yweight, linestyle='none', marker='x', label='CMASS NGC', **plot_kwargs)
 
     if have_bf :
+        if have_delta :
+            with np.load(f'{filebase}/datavectors_fiducials_v0.npz') as f :
+                yfd = np.array([transf_datavec(y_) for y_ in f['data']])
+                std = np.std(yfd, axis=0)
+
         with np.load(f'{filebase}/avg_datavectors_trials.npz') as f :
             trial_data = f['data']
         for k, (v, chisqred) in bf_indices.items() :
             ybf = transf_datavec(trial_data[v])
-            ax.plot(xindices, ybf, label=f'{k} bestfit trial ($\chi^2_{{\sf red}}={chisqred:.2f}$)')
+            l = ax.plot(xindices, ybf, label=f'{k} bestfit trial ($\chi^2_{{\sf red}}={chisqred:.2f}$)')
+            if have_delta :
+                delta = (ybf - yweight) / std
+                print(np.max(np.fabs(delta)))
+                ax_delta.plot(xindices, delta, linestyle='none', marker='o',
+                              color=plt.getp(l[0], 'color'), markersize=1.5)
+
+
+                
     ax.legend(frameon=False, loc='lower center', bbox_to_anchor=(0.5, 1), ncol=4)
 
     _, ymax = ax.get_ylim()
@@ -100,6 +138,8 @@ def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, **p
                 transform=ax.transData, va='top', ha='center')
     for vline in vlines :
         ax.axvline(vline, color='grey')
+        if have_delta :
+            ax_delta.axvline(vline, color='grey')
 
     if pretty_ax :
         ax.axhline(0, color='grey', linestyle='dashed')
@@ -125,8 +165,13 @@ def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, **p
                     label_locs.append((l[1], start_x + ii + (l[0] - x[ii])/(x[ii+1]-x[ii])))
         ax.set_xticks([x[1] for x in tick_locs])
         ax.set_xticklabels([str(x[0]) for x in tick_locs])
+        if have_delta :
+            ax_delta.set_xticks([x[1] for x in tick_locs])
+            ax_delta.set_xticklabels([])
+            ax_delta.xaxis.tick_top()
         for l, loc in label_locs :
-            ax.text(loc, ax.get_ylim()[0]-0.2, l, transform=ax.transData, va='top', ha='center')
+            ax.text(loc, ax.get_ylim()[0]-(0.2+int(have_delta)*0.07), l, transform=ax.transData,
+                    va='top', ha='center')
 
 
         
@@ -135,5 +180,5 @@ def plot_datavec (ax=None, pretty_ax=True, have_bf=False, have_xticks=False, **p
 
 
 if __name__ == '__main__' :
-    fig, ax = plot_datavec(have_bf=True, have_xticks=True)
+    fig, ax = plot_datavec(have_bf=True, have_xticks=True, have_delta=True)
     savefig(fig, f'datavec')
